be3b1e59171efd34f725976756126045ad1ec2ad
zack <zack@zackdeMacBook-Pro.local>
2025-05-13 11:42:26 +0800

    first commit

---
+++ CHANGELOG.md
@@ -0,0 +1,13 @@
+Changelog
+=========
+
+Next Version
+------------
+
+1.0.1 (2025-05-13)
+------------------
+* Change: update example server response data
+
+1.0.0 (2025-05-12)
+------------------
+* Initial release: Forking bunch
---
+++ setup.py
@@ -0,0 +1,24 @@
+# -*- encoding: utf-8 -*-
+from setuptools import find_packages, setup
+
+setup(
+    version="1.0.1",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown; charset=UTF-8",
+    keywords=[
+        "gm",
+        "gm-scaffold",
+        "simplejrpc",
+        "gmssh",
+    ],
+    python_requires=">=3.10",
+    packages=find_packages(),
+    # exclude_package_date={"": [".gitignore"]},
+    install_requires=[
+        "pydispatch==1.1.0",
+        "pyfiglet==1.0.2",
+        "jinja2==3.1.4",
+        "fire==0.7.0",
+        "cleo==2.1.0",
+    ],
+)
