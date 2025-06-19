b6a644bc4fa014f034afbb2b5f5b6a5b125418cd
zack <zack@zackdeMacBook-Pro.local>
2025-06-19 16:42:05 +0800

    fix: remove sample form

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import find_packages, setup
 
 setup(
-    version="1.0.7",
+    version="1.0.8",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown; charset=UTF-8",
     keywords=[
